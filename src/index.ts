503895667e920455714860c75c20c9e40b6aee11
Andres Vizuete <andresv0807@gmail.com>
2025-12-03 14:54:26 -0500

    Modificaciones index.ts: Se elimina excedentes index

--- src/index.ts
+++ src/index.ts
@@ -13,34 +13,5 @@ server.listen(PORT, () => {
 })
 
 
-/* Types e Interfaces */
-
-let exampleString = "Hello, TypeScript!"
-let exampleNumber = 42
-
-type User = {
-  name: string
-  age: number
-  email: string
-}
-
-/* Crear herencia pick para extraer todos los types necesarios*/
-type userEmail = Pick< User, "email" >
-
-
-/* ejemplo con omit */
-interface userEmailOmit {
-  email: Omit< User, "email" >
-}
-
-let dataUser: User = {
-  name: "Alice",
-  age: 30,
-  email: "alice@example.com"
-}
-
-let userEmailData: userEmail = {
-  email: "alice@example.com"
-}
 
 
